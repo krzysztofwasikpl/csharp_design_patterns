f9aae9669dbfaa46d30726ed0b8333f9579256be
Krzysztof Wasik <meetme@krzysztofwasik.pl>
2018-02-03 23:09:27 +0100

    First project. Added gitignore file for vs

---
+++ csharp_design_patterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_design_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("project: home, csharp_design_patterns");
+        }
+    }
+}
